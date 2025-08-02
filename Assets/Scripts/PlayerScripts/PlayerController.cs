9371dcb5dd39cb8d7fcdb75723e37c839ce31d16
ColinRoxs <colin.mcp1029@gmail.com>
2025-08-02 00:10:58 -0400

    Version 0.0.0.6
    Changed Player sprites from individual images into a sprite sheet for better organization. Added mirror to sprite when moving left.

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float hValue = Input.GetAxisRaw("Horizontal");
+        SpriteFlip(hValue);
 
-        rb.linearVelocityX = hValue * 5f;
+        rb.linearVelocityX = hValue * 5f; //adjust mult as nessesary
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * 10f, ForceMode2D.Impulse); //Adjust force as necessary
         }
+    }
+
+    void SpriteFlip(float hValue) //Sprite flipper, might make redundant or modify to make sprite flip to mouse cursor.
+    {
+        if ((hValue > 0 && sr.flipX) || (hValue < 0 && !sr.flipX))
+            sr.flipX = !sr.flipX; // Flip sprite only when direction changes
     }
 }
