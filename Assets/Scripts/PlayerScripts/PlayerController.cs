c8bee6f48395ef5f86c8d10d72269a856e5c106a
ColinRoxs <colin.mcp1029@gmail.com>
2025-08-05 13:13:37 -0400

    0.0.0.12
    Fixed Player getting caught on tiles

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -62,7 +62,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && jumpCount < maxJumpCount)
         {
-            rb.AddForce(Vector2.up * 9f, ForceMode2D.Impulse); //Adjust force as necessary
+            rb.AddForce(Vector2.up * 7f, ForceMode2D.Impulse); //Adjust force as necessary
             jumpCount++;
         }
         isGrounded = Physics2D.OverlapCircle(groundCheckPos, groundCheckRadius, groundLayer);
