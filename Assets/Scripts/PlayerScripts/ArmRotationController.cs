71de3e05bc94c60096b30da982eb6f96a6e022a5
ColinRoxs <colin.mcp1029@gmail.com>
2025-08-10 01:06:26 -0400

    0.0.0.15
    Made the gun properly flip when mouse cursor is to the left of the player.

--- Assets/Scripts/PlayerScripts/ArmRotationController.cs
+++ Assets/Scripts/PlayerScripts/ArmRotationController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class ArmRotationController : MonoBehaviour
 {
@@ -21,6 +20,17 @@ public class ArmRotationController : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(0f, 0f, angleDeg);
         Debug.DrawLine(transform.position, mousePos, Color.white, Time.deltaTime);
+
+        if (mousePos.x > transform.position.x)
+        {
+            //sr.flipY = false;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (mousePos.x < transform.position.x)
+        {
+            //sr.flipY = true;
+            transform.localScale = new Vector3(1, -1, 1);
+        }
 
     }
 
