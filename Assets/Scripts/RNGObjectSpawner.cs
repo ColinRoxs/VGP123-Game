60b7888c0cd7282cdc2b5b9546946cd826c4baff
ColinRoxs <colin.mcp1029@gmail.com>
2025-08-10 01:34:33 -0400

    0.0.0.17
    Nothing of note.

--- Assets/Scripts/RNGObjectSpawner.cs
+++ Assets/Scripts/RNGObjectSpawner.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class RNGObjectSpawner : MonoBehaviour
 {
+    public int RNG; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
