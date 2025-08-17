ba1f6e0f144fb3b0739fa677c51d4d64becd95f7
ColinRoxs <colin.mcp1029@gmail.com>
2025-08-17 12:50:49 -0400

    0.0.0.21
    I got chest spawning working. And all of it is prefabs so all I need to do to add chest functionality, Loot, enemies, and attacks. One step at a time.

--- Assets/Scripts/RNGObjectSpawner.cs
+++ Assets/Scripts/RNGObjectSpawner.cs
@@ -3,16 +3,30 @@ using UnityEngine;
 public class RNGObjectSpawner : MonoBehaviour
 {
     public int RNG; 
+    public GameObject ChestLow;
+    public GameObject ChestHigh;
+    public GameObject ChestMid;
+    public GameObject ChestVeryHigh;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int myRandomInt = Random.Range(1, 11);
+        int myRandomInt = Random.Range(1, 3);
         RNG = myRandomInt;
+
+        if (RNG == 1)
+        {
+            Instantiate(ChestLow, transform.position,transform.rotation);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ChestGen()
+    {
+
     }
 }
