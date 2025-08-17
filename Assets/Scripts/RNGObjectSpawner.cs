85f40bc09d9a1a26279aea339e7e6d90a7ecf6bd
ColinRoxs <colin.mcp1029@gmail.com>
2025-08-16 20:15:57 -0400

    0.0.0.17
    Got the basic of RNG object spawn script working. I have 2 yellow boxes that output a random number from 1-10 and each box outputs their own number.

--- Assets/Scripts/RNGObjectSpawner.cs
+++ Assets/Scripts/RNGObjectSpawner.cs
@@ -6,7 +6,8 @@ public class RNGObjectSpawner : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int myRandomInt = Random.Range(1, 11);
+        RNG = myRandomInt;
     }
 
     // Update is called once per frame
