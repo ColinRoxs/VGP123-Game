5a83274cd59a11aa398fe04ca6e2a3479aec766d
ColinRoxs <colin.mcp1029@gmail.com>
2025-08-17 20:27:12 -0400

    0.0.0.24
    Added a aim shift. Now when holding right mouse button the camera will lean in the direction of the cursor. Has one bug presently with camera z position ever decreasing while holding aim.

--- Assets/Scripts/SmoothCameraFollow.cs
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -4,6 +4,10 @@ public class SmoothCameraFollow : MonoBehaviour
 {
     [SerializeField] private Vector3 offset;
     [SerializeField] private float damping;
+    [SerializeField] private float baseSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float aimShiftStrength;
+
 
     public Transform target;
 
@@ -11,9 +15,27 @@ public class SmoothCameraFollow : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (target == null) return;
+
         Vector3 targetPosition = target.position + offset;
+
+        // Z position lock to retain sidescrolling camera despite needing vec3 movement
         targetPosition.z = transform.position.z;
 
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref vel, damping);
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = transform.position.z;
+
+            Vector3 aimDirection = (mouseWorldPos - target.position).normalized;
+            Vector3 aimShift = aimDirection * aimShiftStrength;
+            targetPosition += aimShift;
+        }
+
+        float distance = Vector3.Distance(transform.position, targetPosition);
+        float dynamicDamping = Mathf.Clamp(baseSpeed + distance * damping, baseSpeed, maxSpeed);
+
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref vel, damping);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref vel, 1f / dynamicDamping);
     }
 }
