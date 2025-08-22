bad48d35898bb22c9efb24362844876896002652
ColinRoxs <colin.mcp1029@gmail.com>
2025-08-22 03:27:13 -0400

    0.0.0.32
    Added the functions for items spawning from chests. Just need to get the item pick up working and the buffs working as well.

--- Assets/Scripts/ChestController.cs
+++ Assets/Scripts/ChestController.cs
@@ -4,6 +4,15 @@ public class ChestController : MonoBehaviour
 {
     [SerializeField] private bool canOpenChest = false;
     private Animator chestAnimator;
+
+    public int RNG;
+    public Transform itemSpawnPoint;
+    public GameObject Item1;
+    public GameObject Item2;
+    public GameObject Item3;
+    public GameObject Item4;
+    public float spawnForce = 300f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +21,9 @@ public class ChestController : MonoBehaviour
         {
             Debug.LogWarning("Error! chest animator not present.");
         }
+
+        int myRandomInt = Random.Range(1, 2);
+        RNG = myRandomInt;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -30,6 +42,29 @@ public class ChestController : MonoBehaviour
         }
     }
 
+    public void ChestOpen()
+    {
+        GameObject spawnedItem = null;
+
+        if (RNG == 1)
+        {
+            spawnedItem = Instantiate(Item1, itemSpawnPoint.position, Quaternion.identity);
+        }
+        if (RNG == 2)
+        {
+            spawnedItem = Instantiate(Item2, itemSpawnPoint.position, Quaternion.identity);
+        }
+        if (RNG == 3)
+        {
+            spawnedItem = Instantiate(Item3, itemSpawnPoint.position, Quaternion.identity);
+        }
+        if (RNG == 4)
+        {
+            spawnedItem = Instantiate(Item4, itemSpawnPoint.position, Quaternion.identity);
+        }
+
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -37,6 +72,8 @@ public class ChestController : MonoBehaviour
         {
             chestAnimator.SetTrigger("IsOpen");
             Debug.Log("E Pressed and can open chest!");
+            ChestOpen();
         }
     }
+
 }
--- Assets/Scripts/ItemTrailT1script.cs
+++ Assets/Scripts/ItemTrailT1script.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class ItemTrailT1script : MonoBehaviour
 {
+    private LayerMask groundLayer;
+    public int RNG;
+    public GameObject Item1;
+
+    private bool hasLanded = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+        groundLayer = LayerMask.GetMask("GroundColliderLayer");
+
+        if (groundLayer == 0)
+        {
+            Debug.LogWarning("Ground layer not set. Please set the Ground layer in LayerMask.");
+        }
+
+        int myRandomInt = Random.Range(1, 2);
+        RNG = myRandomInt;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (!hasLanded && ((1 << collision.gameObject.layer) & groundLayer) != 0)
+        {
+            hasLanded = true;
+
+            if (Item1 != null)
+            {
+                Instantiate(Item1, transform.position, Quaternion.identity);
+            }
+
+            Destroy(gameObject);
+        }
     }
 }
